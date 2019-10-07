1213753df3de058409214405087ef216fde44c25
v-sopin <sopin.02.09.21@gmail.com>
2019-10-07 19:22:42 +0300

    initial

---
+++ scripts/config.py
@@ -0,0 +1,13 @@
+TOKEN = '981914890:AAGLUjSIhc4H3qLWxTjAzWxFk1Y6HnFcEFg' # '811665420:AAGp4ZuOfYtT97-iEksqWYIRMYw6cwHvn1k'
+
+DB_NAME = 'telegrambot'
+DB_PASSWORD = 'Noe9HSLWyXZT'
+DB_HOST = 'localhost'
+DB_USER = 'root'
+
+BOT_MAIL = 'service.marketbot@gmail.com'
+BOT_MAIL_PASSWORD = 'service-market'
+MAIL_RECEPIENT = 'info@service-market.com.ua'
+
+ADMINS = [84448339, ]
+DEVELOPER_ID = 326469336
